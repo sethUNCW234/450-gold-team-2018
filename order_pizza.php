09f526f56640b9095d20b03d85322fa6b61857b5
Seth <saf5779@uncw.edu>
2018-04-08 15:54:21 -0400

    order page

---
+++ order_pizza.php
@@ -0,0 +1,51 @@
+
+<?php 
+require 'header.php';
+?>
+
+<div class ="row">
+<div class="col-sm">
+<h2>Order here</h2>
+</div>
+<div class= "col-sm" style="padding-right:300px;">
+<h2>Your Total so far: $0.00</h2>
+</div>
+
+</div>
+
+
+
+<br/>
+<h3>Select your crust type</h3>
+<div class="form-check">
+    <input class="form-check-input" name="group100" type="radio" id="radio100">
+    <label class="form-check-label" for="radio100">Option 1</label>
+</div>
+
+<div class="form-check">
+    <input class="form-check-input" name="group100" type="radio" id="radio101" checked>
+    <label class="form-check-label" for="radio101">Option 2</label>
+</div>
+
+<div class="form-check">
+    <input class="form-check-input" name="group100" type="radio" id="radio102">
+    <label class="form-check-label" for="radio102">Option 3</label>
+</div>
+<br/>
+<h3>Toppings</h3>
+ <div class="row">
+  <div class="col-md-12">
+  <div class="checkbox">
+    <label><input type="checkbox" value=""> Pepperoni</label>
+  </div>
+    <div class="checkbox">
+    <label><input type="checkbox" value=""> Sausage</label>
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox" value=""> Ham</label>
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox" value=""> Bacon</label>
+  </div>
+ </div>
+</div>
