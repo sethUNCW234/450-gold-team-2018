c7fa563890832641a106c1178ae0b95e0264582e
Seth <saf5779@uncw.edu>
2018-04-08 11:58:05 -0400

    added a possible homepage

---
+++ golds_home_page_idea.php
@@ -0,0 +1,49 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+<nav class="navbar navbar-light" style="background-color: #f35353">
+  <a class="navbar-brand" style="color:gold" href="#">Gold's Pizza </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Features</a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+    </div>
+  </div>
+</nav>
+<br/>
+<div>
+<blockquote class="blockquote text-center">
+  <p class="mb-0">Wilmington's New Premier Pizza Place</p>
+  
+</blockquote>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-75" src="http://wwhouston.midnightrodeoamarillo.com/wp-content/uploads/sites/10/2015/08/happy-hour-6-15-662x1024-662x1024.jpg" style="height:200px;width:300px;" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-75" src="https://media-cdn.tripadvisor.com/media/photo-s/08/02/ea/5b/mon-and-tues-free-drink.jpg" style="height:200px;width:300px;" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-75" src="https://ze0axagqvu-flywheel.netdna-ssl.com/wp-content/uploads/edd/2016/08/Back-To-School-Drink-Special-Flyer-Template-FH-2.jpg" style="height:200px;width:300px;" alt="Third slide">
+    </div>
+  </div>
+</div>
+</div>
+<div class="row">
+	<img src="https://images.pexels.com/photos/724216/pexels-photo-724216.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb" style="height:300px;width:350px;" />
+ </div>
