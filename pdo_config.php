750d661805dd7a56d05d6d8c62a22b967596c1a3
sethUNCW234 <saf5779@uncw.edu>
2018-04-24 22:16:40 -0400

    Update pdo_config.php

--- pdo_config.php
+++ pdo_config.php
@@ -1,8 +1,12 @@
 <?php
-/*Sam Falcon*/
-		define(DBCONNSTRING,'mysql:host=localhost;dbname=yourUNCWemail'); /* your email without the @: just the letters and numbers */
-		define(DBUSER, '???####');
-		define(DBPASS,'????'); //database password (might be different from your UNCW one)
+/*Sam Falcon*/ //database password (might be different from your UNCW one)
+
++ 		define(DBCONNSTRING,'mysql:host=127.0.0.1;dbname=pizza'); /* your email without the @: just the letters and numbers */
+
++ 		define(DBUSER, 'root');
+
++ 		define(DBPASS,'root'); //database password (might be different from your UNCW one)
+
 		try {
 			$conn= new PDO(DBCONNSTRING, DBUSER, DBPASS);
 		} catch (PDOException $e) {
