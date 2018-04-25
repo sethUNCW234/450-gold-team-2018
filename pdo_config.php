487de0294917847daf55e9b4161e08832d7b4044
sethUNCW234 <saf5779@uncw.edu>
2018-04-24 22:23:41 -0400

    Update pdo_config.php

--- pdo_config.php
+++ pdo_config.php
@@ -1,34 +1,20 @@
 <?php
-<<<<<<< HEAD
-/*Sam Falcon*/
-<<<<<<< Updated upstream
-		define(DBCONNSTRING,'mysql:host=localhost;dbname=yourUNCWemail'); /* your email without the @: just the letters and numbers */
-		define(DBUSER, '???####');
-		define(DBPASS,'????'); //database password (might be different from your UNCW one)
-=======
-		 //database password (might be different from your UNCW one)
-		define(DBCONNSTRING,'mysql:host=127.0.0.1;dbname=pizza'); /* your email without the @: just the letters and numbers */
-		define(DBUSER, 'root');
-		define(DBPASS,'root'); //database password (might be different from your UNCW one)
->>>>>>> Stashed changes
-=======
+
 /*Sam Falcon*/ //database password (might be different from your UNCW one)
 
-+ 		define(DBCONNSTRING,'mysql:host=127.0.0.1;dbname=pizza'); /* your email without the @: just the letters and numbers */
+		define(DBCONNSTRING,'mysql:host=127.0.0.1;dbname=pizza'); /* your email without the @: just the letters and numbers */
 
-+ 		define(DBUSER, 'root');
+		define(DBUSER, 'root');
 
-+ 		define(DBPASS,'root'); //database password (might be different from your UNCW one)
+		define(DBPASS,'root'); //database password (might be different from your UNCW one)
 
->>>>>>> 750d661805dd7a56d05d6d8c62a22b967596c1a3
+
 		try {
 			$conn= new PDO(DBCONNSTRING, DBUSER, DBPASS);
 		} catch (PDOException $e) {
 	echo $e->getMessage();
 		}
-<<<<<<< HEAD
+
 		
 ?>
-=======
-?>
->>>>>>> 750d661805dd7a56d05d6d8c62a22b967596c1a3
+
